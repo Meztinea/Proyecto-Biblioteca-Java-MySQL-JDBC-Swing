bed2dd20f645b0a0613c77c5318ec4bd19091dad
Raquel Martínez <meztinea.dev@gmail.com>
2024-02-14 15:34:31 -0600

    Método para retornar resultado de SELECT SQL a controladora lógica

--- biblioteca/src/main/java/mx/com/biblioteca/persistencia/ControladoraPersistencia.java
+++ biblioteca/src/main/java/mx/com/biblioteca/persistencia/ControladoraPersistencia.java
@@ -3,7 +3,7 @@ package mx.com.biblioteca.persistencia;
 
 import java.sql.SQLException;
 import mx.com.biblioteca.logica.Libro;
-
+import java.sql.ResultSet;
 /**
  *
  * @author Raquel Martínez
@@ -11,6 +11,11 @@ import mx.com.biblioteca.logica.Libro;
 public class ControladoraPersistencia {
     
     LibroP libroP = new LibroP();
+    
+    public ResultSet consultarLibros() throws SQLException {
+        // Enviar el tipo resulset a la lógica
+        return LibroP.consultarLibros();
+    }
     
     public void guardarLibro(Libro libro) throws SQLException {
         libroP.guardar(libro);
